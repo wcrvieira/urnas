58a9cc2cbbca69823ee391b64a1d59349029e521
wagnervieira <40812183+wcrvieira@users.noreply.github.com>
2026-03-20 09:57:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Validação de percentuais de uma eleição
+// Criação e inicialização das variáveis
+int votos, brancos, nulos = 0;
+
+// Solicitação de dados do usuário
+System.Console.WriteLine("*** Apuração de votos do município ***"); ;
+System.Console.Write("Informe a quantidade de votos válidos: ");
+votos = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Informe o total de votos em branco: ");
+brancos = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Informe o total de votos nulos: ");
+nulos = Convert.ToInt32(Console.ReadLine());
+
+// Condição de existência de triângulo
+// A soma de dois lados quaisquer deve ser sempre maior que o terceiro lado
+System.Console.WriteLine($"Total de {votos} votos válidos, {brancos} votos em branco e {nulos} votos nulos");
